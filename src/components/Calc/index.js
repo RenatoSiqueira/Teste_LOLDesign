01f91907a93c2a41643db51ee06f480e07836d2d
Renato Siqueira <renatoelysiqueira@gmail.com>
2020-07-17 15:41:05 -0300

    new sections

--- src/components/Calc/index.js
+++ src/components/Calc/index.js
@@ -1,72 +1,54 @@
 import React from 'react'
 
 const Calc = () => (
-    <div class="flex flex-wrap -mx-3 mb-2">
-        <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                Origem
-            </label>
-            <div class="relative">
-                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                    <option>(011) - São Paulo</option>
-                    <option>(016) - São Paulo</option>
-                    <option>(017) - São Paulo</option>
-                    <option>(018) - São Paulo</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+    <section className="text-gray-700 body-font">
+        <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-12">
+                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Calculadora</h1>
+                <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep.</p>
+            </div>
+            <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:px-0 justify-around text-center">
+                <div className="relative">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                        Origem
+                        </label>
+                    <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option>(011) - São Paulo</option>
+                        <option>(016) - São Paulo</option>
+                        <option>(017) - São Paulo</option>
+                        <option>(018) - São Paulo</option>
+                    </select>
+                </div>
+                <div className="relative">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                        Destino
+                        </label>
+                    <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option>(011) - São Paulo</option>
+                        <option>(016) - São Paulo</option>
+                        <option>(017) - São Paulo</option>
+                        <option>(018) - São Paulo</option>
+                    </select>
+                </div>
+                <div className="relative">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+                        Tempo de Ligação
+                        </label>
+                    <input className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        placeholder="Ex: 60 para 1 Hora" type="text" />
+                </div>
+                <div className="relative">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+                        Plano
+                    </label>
+                    <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option>Com FaleMais</option>
+                        <option>Sem FaleMais</option>
+                    </select>
                 </div>
             </div>
         </div>
-        <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                Destino
-            </label>
-            <div class="relative">
-                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                    <option>(011) - São Paulo</option>
-                    <option>(016) - São Paulo</option>
-                    <option>(017) - São Paulo</option>
-                    <option>(018) - São Paulo</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                </div>
-            </div>
-        </div>
-        <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                Tempo de Ligação
-            </label>
-            <div class="relative">
-                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                    <option>(011) - São Paulo</option>
-                    <option>(016) - São Paulo</option>
-                    <option>(017) - São Paulo</option>
-                    <option>(018) - São Paulo</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                </div>
-            </div>
-        </div>
-        <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                Plano
-        </label>
-            <div class="relative">
-                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                    <option>(011) - São Paulo</option>
-                    <option>(016) - São Paulo</option>
-                    <option>(017) - São Paulo</option>
-                    <option>(018) - São Paulo</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                </div>
-            </div>
-        </div>
-    </div>
+    </section>
 )
 
 export default Calc
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,10 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Table from '../components/Table'
 import Calc from '../components/Calc'
+import CTA from '../components/CTA'
+import Pricing from '../components/Pricing'
+import Testimonials from '../components/Testimonials'
+import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 
 const Index = () => (
@@ -13,6 +17,10 @@ const Index = () => (
         <Hero />
         <Table />
         <Calc />
+        <CTA />
+        <Pricing />
+        <Testimonials />
+        <Contact />
         <Footer />
     </div>
 )
